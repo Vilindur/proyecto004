4ec0096b4c1c0e6020ceb84323cbff96882eaaec
Vilindur <alepedi01@gmail.com>
2025-02-19 09:17:24 +0100

    Proyecto4_Acabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto004';
+  valor1!:number;
+  valor2!:number;
+  valor3!:number;
+  resultado = ""; //Aqui volcamos el resultado del dado
+  constructor() {
+    this.lanzarDados();
+  }
+
+  generarAleatorio() {
+    return Math.floor(Math.random()*6)+1;
+  }
+
+  lanzarDados() {
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+    this.comprobarResultadoJuego();
+  }
+
+  comprobarResultadoJuego() {
+    if((this.valor1 == this.valor2) && (this.valor1 == this.valor3)) {
+      this.resultado = "Has ganado";
+    } else {
+      this.resultado = "Has perdido";
+    }
+  }
 }
